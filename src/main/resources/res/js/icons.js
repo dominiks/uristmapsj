a4ea94a39be69fbf897f0224d408482221ee24f8
dominiks <domschacht@gmail.com>
2015-06-12 16:05:45 +0200

    fixed site icons wandering around when zooming

--- src/main/resources/res/js/icons.js
+++ src/main/resources/res/js/icons.js
@@ -4,8 +4,8 @@ function get_icon(url) {
   if (icons[url] === undefined) {
     var icon = L.icon({
       "iconUrl" : url,
-      //"iconSize": [35, 35],
-      //"iconAnchor": [17, 17]
+      "iconSize": [35, 35],
+      "iconAnchor": [17, 17]
     });
     icons[url] = icon;
   }
